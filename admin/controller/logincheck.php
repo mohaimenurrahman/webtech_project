b0b80f3f1b5dda368585c590024d2aefee9a5b0d
Aunoy <hasanmahmudul2457@gmail.com>
2021-11-27 20:20:43 +0600

    Login Successfull

--- admin/controller/logincheck.php
+++ admin/controller/logincheck.php
@@ -6,21 +6,33 @@
 	if(isset($_POST['submit'])){
 		$username = $_POST['username'];
 		$password = $_POST['password'];
+		$type = $_POST['type'];
+
 
 		if($username != ""){
 			if($password != ""){
+				if($_POST['type']!= ""){
+					$status = validate($username, $password);
+				    if($status){
+				        if($type == "admin"){
+				           setcookie('flag', 'true', time()+3600, '/');
+				           //setcookie('loggedInName',$username,time()+3600,'/');
+                           //setcookie('loggedInId',$username,time()+3600,'/');
+				           header('location: ../views/home.php');
 
-               $status = validate($username, $password);
-				
-				if($status){
-				    setcookie('flag', 'true', time()+3600, '/');
-				    //setcookie('loggedInName',$username,time()+3600,'/');
-                    //setcookie('loggedInId',$username,time()+3600,'/');
-				    header('location: ../views/home.php');
+				        }else if($_POST['type'] == "placementOfficer"){
+
+					    }else if($_POST['type'] == "jobProvider"){
+
+					    }else{
+
+					    }
+				    }else{
+					    echo "invalid username/password";
+					}    
 				}else{
-					echo "invalid username/password";
-				}
-                
+					echo "Missing Type";
+				}  
 			}else{
 				echo "Invalid password...";
 			}
