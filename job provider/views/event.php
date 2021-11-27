6f8b49b470fd123e105dc5e4887ec20f8aca3514
mohaimenurrahman <mohaimenur888@gmail.com>
2021-11-28 00:15:18 +0600

    event

---
+++ job provider/views/event.php
@@ -0,0 +1,71 @@
+<?php 
+  include('../views/header.php');
+?>
+<html>
+<head>
+  <title>Event</title>
+  <style>
+fieldset {
+  background-color: #eeeeee;
+  width:70px ;
+  text-align:center;
+ 
+}
+
+legend {
+  background-color: gray;
+  color: white;
+  padding: 5px 10px;
+  width:80px;
+
+}
+
+input {
+  margin: 10px;
+}
+
+textarea {
+  resize: none;
+}
+
+
+</style>
+</head>
+<body>
+  
+  <form method="post" action="../controller/eventcheck.php">
+    <fieldset>
+      <legend>EVENT</legend>
+      <a href="../views/home.php">Back |</a>
+      <a href="../controller/logout.php">Logout</a>
+      <table>
+        <tr>
+          <td>Event Name</td>
+          <td><input type="text" name="name" value=""></td>
+        </tr>
+        <tr>
+          <td>Date</td>
+          <td><input type="date" name="date" value=""><br></td>
+        </tr>
+       
+        <tr>
+          <td>Description</td>
+          <td><textarea id="text" name="description" rows="10" cols="50"></textarea></td>
+        </tr>
+
+        <tr>
+          <td></td>
+          <td align="right"><input type="submit" name="submit" value="Post"></td>
+        </tr>
+      </table>
+      
+    </fieldset>
+  </form>
+
+</body>
+</html>
+
+
+
+
+
