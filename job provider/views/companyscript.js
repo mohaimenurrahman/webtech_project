8c1489a21fecc89004a5129ad1d1eb2408b4c875
mohaimenurrahman <mohaimenur888@gmail.com>
2021-11-28 00:02:27 +0600

    company

---
+++ job provider/views/companyscript.js
@@ -0,0 +1,19 @@
+"use strict"
+
+
+function ajax(){
+
+	let name = document.getElementById('name').value;
+	let xhttp= new XMLHttpRequest();
+	xhttp.open('POST', 'company.php', true);
+	xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	xhttp.send('name='+name);
+
+	xhttp.onreadystatechange = function(){
+
+		if(this.readyState == 4 && this.status == 200){
+			document.getElementById('d2').innerHTML = this.responseText;		
+		}
+	}
+
+}
