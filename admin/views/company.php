23e0f84dfca9c009522d2ddcf4a3948814cc18f7
Aunoy <hasanmahmudul2457@gmail.com>
2021-12-05 22:35:17 +0600

    Company Search by AJAX

--- admin/views/company.php
+++ admin/views/company.php
@@ -18,6 +18,17 @@
         <a href="../controller/logout.php"> Log Out</a>
     </center>
     <br><br><br> 
+
+    <center>
+        <input type="text" id="d1" name="name" value="" onkeyup="ajax()" />
+          <input type="button" name="click" value="Search" onclick="ajax()" />
+     </center>
+
+     <div id="d2" align="right" width="500">
+        
+     </div>
+
+     <br><br><br> <br>
  
     <table border="1" align="center">
         <tr>
@@ -47,5 +58,8 @@
     ?>  
         
     </table>
+
+    <script type="text/javascript" src="../asset/companyscript.js"></script>
+
 </body>
 </html>
